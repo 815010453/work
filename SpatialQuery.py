574fbab6c7f6e0a730db9066eb31b6b6ddbcc9cb
815010453 <815010453@qq.com>
2022-05-26 22:51:59 +0800

    update all

--- SpatialQuery.py
+++ SpatialQuery.py
@@ -93,7 +93,7 @@ class QuadTree(object):
                 return key
         return ''
 
-    def window_query(self, rect: Rectangle) -> ' list[Point]':
+    def window_query(self, rect: Rectangle) -> 'list[Point]':
         res = []
         if not self.rect.is_disjoin(rect):
             if self.children['NW'] is not None:
@@ -107,17 +107,20 @@ class QuadTree(object):
 
 
 def main(argv):
+    task = 0
+    input_file = ''
+    test_file = ''
     try:
-        input_file = argv[0]
-        task = str(argv[1])
-        test_file = argv[2]
+        input_file = str(argv[0])
+        task = int(argv[1])
+        test_file = str(argv[2])
     except IndexError:
         print("The input arguments are in correct")
         return None
     finally:
         if not os.path.exists('outputs'):
             os.mkdir('outputs')
-        if task == "1":
+        if task == 1:
             """task one: build kd(2-d)-tree  for query"""
             df_dataset = pd.read_table(str(input_file), sep=',', usecols=[0, 1, 2])
             df_test = pd.read_table(str(test_file), header=None, sep=' ', names=['longitude', 'latitude', 'k'])
@@ -158,7 +161,7 @@ def main(argv):
                     for v in indices:
                         f.write(str(v))
                         f.write('\n')
-        elif task == "2":
+        elif task == 2:
             "task two: build the kd(2-d)-tree by case"
             df_dataset = pd.read_table(str(input_file), sep=',')
             df_test = pd.read_table(str(test_file), header=None, sep=' +',
@@ -174,26 +177,28 @@ def main(argv):
             df_test.day_start = df_test.day_start.str.replace('"', '')
             df_test.time_start = df_test.time_start.str.replace('"', '')
             # concat
-            df_test['datatime_begin'] = df_test.day_start + '-' + df_test.time_start
-            df_test.datatime_begin = pd.to_datetime(df_test.datatime_begin)
+            df_test['datetime_begin'] = df_test.day_start + '-' + df_test.time_start
+            df_test.datetime_begin = pd.to_datetime(df_test.datetime_begin)
             df_test.day_end = df_test.day_end.str.replace('"', '')
             df_test.time_end = df_test.time_end.str.replace('"', '')
             df_test.time_end = df_test.time_end.str.replace(')', '', regex=True)
-            df_test['datatime_end'] = df_test.day_end + '-' + df_test.time_end
-            df_test.datatime_end = pd.to_datetime(df_test.datatime_end)
+            df_test['datetime_end'] = df_test.day_end + '-' + df_test.time_end
+            df_test.datetime_end = pd.to_datetime(df_test.datetime_end)
             test = df_test[['longitude', 'latitude']].to_numpy(dtype=float)
-            df_dataset['datatime'] = df_dataset.date + '-' + df_dataset.time + ':00'
-            df_dataset.datatime = pd.to_datetime(df_dataset.datatime)
+            df_dataset['datetime'] = df_dataset.date + '-' + df_dataset.time + ':00'
+            df_dataset.datetime = pd.to_datetime(df_dataset.datetime)
+            del df_dataset['date']
+            del df_dataset['time']
             k = df_test['k'].to_numpy()
             j = 0
             for t in k:
                 temp_test_coord = test[j]
-                temp_test_datatime_begin = df_test['datatime_begin'][j]
-                temp_test_datatime_end = df_test['datatime_end'][j]
+                temp_test_datetime_begin = df_test['datetime_begin'][j]
+                temp_test_datetime_end = df_test['datetime_end'][j]
                 drop_indices = []
                 for id_index, v_id in enumerate(df_dataset['id']):
-                    if df_dataset['datatime'][id_index] < temp_test_datatime_begin or \
-                            df_dataset['datatime'][id_index] > temp_test_datatime_end:
+                    if df_dataset['datetime'][id_index] < temp_test_datetime_begin or \
+                            df_dataset['datetime'][id_index] > temp_test_datetime_end:
                         drop_indices.append(id_index)
                 temp_dataset = df_dataset.drop(drop_indices)
                 if temp_dataset.empty:
@@ -226,7 +231,7 @@ def main(argv):
                         f.write(str(v))
                         f.write('\n')
                 j += 1
-        elif task == "3":
+        elif task == 3:
             """Task 3: build quadtree for query"""
             df_dataset = pd.read_table(str(input_file), sep=',', usecols=[0, 1, 2])
             df_test = pd.read_table(str(test_file), header=None, sep=' ',
@@ -284,8 +289,7 @@ def main(argv):
                     for v in real_res:
                         f.write(str(v))
                         f.write('\n')
-
-        elif task == "4":
+        elif task == 4:
             """Task 4: build quadtree for query and construct result by case"""
             df_dataset = pd.read_table(str(input_file), sep=',')
             df_test = pd.read_table(str(test_file), header=None, sep=' +',
@@ -298,24 +302,27 @@ def main(argv):
             df_test.latitude2 = df_test.latitude2.str.replace(")", "", regex=True)
             df_test.latitude2 = pd.to_numeric(df_test.latitude2)
             test_coord2 = df_test[['longitude2', 'latitude2']].to_numpy(dtype=float)
+            # dataset
             dataset = df_dataset[['longitude', 'latitude']].to_numpy(dtype=float)
             s_id = df_dataset['id'].to_numpy(dtype=int)
-            df_dataset['datatime'] = df_dataset.date + '-' + df_dataset.time + ':00'
-            df_dataset.datatime = pd.to_datetime(df_dataset.datatime)
+            df_dataset['datetime'] = df_dataset.date + '-' + df_dataset.time + ':00'
+            df_dataset.datetime = pd.to_datetime(df_dataset.datetime)
 
             df_test.day_start = df_test.day_start.str.replace('(', '', regex=True)
             df_test.day_start = df_test.day_start.str.replace('"', '')
             df_test.time_start = df_test.time_start.str.replace('"', '')
-            df_test['datatime_begin'] = df_test.day_start + '-' + df_test.time_start
-            df_test.datatime_begin = pd.to_datetime(df_test.datatime_begin)
+            df_test['datetime_begin'] = df_test.day_start + '-' + df_test.time_start
+            df_test.datetime_begin = pd.to_datetime(df_test.datetime_begin)
 
             df_test.day_end = df_test.day_end.str.replace('"', '')
             df_test.time_end = df_test.time_end.str.replace('"', '')
             df_test.time_end = df_test.time_end.str.replace(')', '')
-            df_test['datatime_end'] = df_test.day_end + '-' + df_test.time_end
-            df_test.datatime_end = pd.to_datetime(df_test.datatime_end)
-            print(df_test.datatime_begin)
-            print(df_test.datatime_end)
+            df_test['datetime_end'] = df_test.day_end + '-' + df_test.time_end
+            df_test.datetime_end = pd.to_datetime(df_test.datetime_end)
+            del df_dataset['date']
+            del df_dataset['time']
+            print(df_test.datetime_begin)
+            print(df_test.datetime_end)
 
             x_max, y_max = np.max(dataset, axis=0)
             x_min, y_min = np.min(dataset, axis=0)
@@ -341,8 +348,8 @@ def main(argv):
             j = 0
             for temp_coord1 in test_coord1:
                 temp_coord2 = test_coord2[j]
-                temp_test_datatime_begin = df_test['datatime_begin'][j]
-                temp_test_datatime_end = df_test['datatime_end'][j]
+                temp_test_datetime_begin = df_test['datetime_begin'][j]
+                temp_test_datetime_end = df_test['datetime_end'][j]
                 window_coord = np.array([temp_coord1, temp_coord2])
                 x_max, y_max = np.max(window_coord, axis=0)
                 x_min, y_min = np.min(window_coord, axis=0)
@@ -358,23 +365,66 @@ def main(argv):
                             real_res.extend(k)
                 real_res = np.array(real_res)
                 real_res = np.unique(real_res)
-                # query by datatime
-                real_datatime_res = []
+                # query by datetime
+                real_datetime_res = []
                 for i in real_res:
                     id_index = np.argwhere(s_id == i)[0]
-                    if df_dataset['datatime'][id_index[0]] >= temp_test_datatime_begin or \
-                            df_dataset['datatime'][id_index] <= temp_test_datatime_end:
-                        real_datatime_res.append(i)
-                print(real_datatime_res)
+                    if df_dataset['datetime'][id_index[0]] >= temp_test_datetime_begin or \
+                            df_dataset['datetime'][id_index] <= temp_test_datetime_end:
+                        real_datetime_res.append(i)
+                print(real_datetime_res)
                 j += 1
                 # write file
                 with open('outputs/task4_sample_results.txt', 'a', encoding='utf-8') as f:
-                    for v in real_datatime_res:
+                    for v in real_datetime_res:
                         f.write(str(v))
                         f.write('\n')
+        elif task == 5:
+            """Task 5: build quadtree for query and construct result by case"""
+            df_dataset = pd.read_table(str(input_file), sep=',')
+            dataset = df_dataset[['longitude', 'latitude']].to_numpy(dtype=float)
+            s_id = df_dataset['id'].to_numpy(dtype=int)
+            df_dataset['datetime'] = df_dataset.date + '-' + df_dataset.time + ':00'
+            df_dataset.datetime = pd.to_datetime(df_dataset.datetime)
+            del df_dataset['date']
+            del df_dataset['time']
+            df_test = pd.read_table(str(test_file), header=None, sep=r'"', names=['coord_group', 'date', 'distance'])
+            df_test.coord_group = df_test.coord_group.str.replace('(', '', regex=True)
+            df_test.coord_group = df_test.coord_group.str.replace(')', '', regex=True)
+            df_test.date = pd.to_datetime(df_test.date)
+            df_test.distance = pd.to_numeric(df_test.distance)
+            dis = df_test['distance'].to_numpy(dtype=float)
+            j = 0
+            # construct quadtree
+            x_max, y_max = np.max(dataset, axis=0)
+            x_min, y_min = np.min(dataset, axis=0)
+            rect_all = Rectangle(x_min, y_min, x_max, y_max)
+            unique_dataset, unique_index, dataset_inverse, dataset_count = np.unique(dataset, axis=0,
+                                                                                     return_index=True,
+                                                                                     return_counts=True,
+                                                                                     return_inverse=True)
+            q_tree = QuadTree(rect_all, [])
+            for index, coord in enumerate(unique_dataset):
+                attr = {'id': s_id[unique_index[index]]}
+                temp = Point(coord[0], coord[1], attr)
+                q_tree.insert(temp)
+            repeat_coord = []
+            for index, i in enumerate(unique_index):
+                if dataset_count[index] > 1:
+                    temp_coord = []
+                    idx = np.where(dataset_inverse == index)
+                    for j in idx[0]:
+                        temp_coord.append(s_id[j])
+                    repeat_coord.append(temp_coord)
 
-        #   elif task == 5":
-        """Task 4: build quadtree for query and construct result by case"""
+            for d in dis:
+                coord_group_str = str(df_test['coord_group'][j]).strip().split(' ')
+                coord_group = []
+                for i in range(0, len(coord_group_str) - 1, 2):
+                    temp_coord = [float(coord_group_str[i]), float(coord_group_str[i + 1])]
+                    coord_group.append(temp_coord)
+                print(coord_group)
+                j += 1
 
 
 if __name__ == '__main__':
